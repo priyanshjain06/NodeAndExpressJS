4e3d3763a2e2eafec3bbe6c44f566aa551dda5ae
Priyansh Jain <priyansh01jain@gmail.com>
2026-02-25 15:19:06 +0530

    added comments

--- pathModule/pathMethods.js
+++ pathModule/pathMethods.js
@@ -1,13 +1,13 @@
-const express = require ('express')
+const express = require('express')
 const os = require('os')
-const path = require ('path')
+const path = require('path')
 
 
-console.log(path.dirname('C:/Users/Desktop/batchfirstNode')) ;
+console.log(path.dirname('C:/Users/Desktop/batchfirstNode'));
 // will return the parent directory.
 
 
-console.log(path.extname('C:/Users/Desktop/batchfirstNode/index.js')) ;
+console.log(path.extname('C:/Users/Desktop/batchfirstNode/index.js'));
 // Purpose: Returns the extension of the file, including the dot.
 
 
@@ -17,9 +17,9 @@ console.log(path.basename('C:/Users/Desktop/batchfirstNode/index.js'))
 console.log(path.parse('C:/Users/Desktop/batchfirstNode/index.js'))
 // Returns an object with the root, directory, base, name, and ext properties of the path.
 
-console.log(path.join('Users','local' , 'bin' , 'index.js'))
-// gives relative path
+console.log(path.join('Users', 'local', 'bin', 'index.js'))
+//REVIEW  gives relative path not always but 
 
-console.log(path.resolve('Users','local' , 'bin' , 'index.js'))
-// gives absolute path , it is a complete path
+console.log(path.resolve('Users', 'local', 'bin', 'index.js'))
+//REVIEW always  gives absolute path , it is a complete path
 
